85a6729bcc74f5a8efc9de94a478209faa864528
Brayan Garcia <bgarcia.95@outlook.com>
2022-05-09 17:36:34 -0600

    add global styles files

---
+++ src/theme/globalStyles.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  img: {
+    width: '100%',
+    height: '100%',
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default globalStyles;
