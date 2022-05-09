3ac19cdf7b036deecca7f6ecc1b3904ac9ecdb3b
Brayan Garcia <bgarcia.95@outlook.com>
2022-05-09 17:38:05 -0600

    create basic screens

---
+++ src/screens/pokemon/PokemonDetailsScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const PokemonDetailsScreen = () => {
+  return (
+    <View>
+      <Text>PokemonDetailsScreen</Text>
+    </View>
+  );
+};
