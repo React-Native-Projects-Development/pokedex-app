180d48a12509832f5a0750948527afa55d942054
Brayan Garcia <bgarcia.95@outlook.com>
2022-05-08 20:13:39 -0600

    create custom text component

---
+++ src/components/CustomText.tsx
@@ -0,0 +1,147 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+import fonts from '~config/fonts';
+import colors from '~theme/colors';
+import {hp, wp} from '~theme/metrics';
+
+type Variants = {
+  body: 'body';
+  tabTitleActive: 'tabTitleActive';
+  tabTitleInactive: '  tabTitleInactive';
+  header: 'header';
+  search: 'search';
+  card: 'card';
+  link: 'link';
+  newsTitle: 'newsTitle';
+  newsSubtitle: 'newsSubtitle';
+  tag: 'tag';
+  tagBold: 'tagBold';
+  number: 'number';
+  pokemonType: 'pokemonType';
+};
+
+interface Props {
+  variant?: keyof Variants;
+}
+
+export const CustomText: React.FC<Props> = ({children, variant = 'body'}) => {
+  return <Text style={styles[variant]}>{children}</Text>;
+};
+
+const styles = StyleSheet.create({
+  header: {
+    fontFamily: fonts.CircularBlack,
+    fontSize: wp(30),
+    lineHeight: hp(42),
+    color: colors.textTitle,
+  },
+  search: {
+    fontFamily: fonts.CircularBook,
+    fontSize: wp(14),
+    fontWeight: '400',
+    lineHeight: hp(42),
+    letterSpacing: wp(0),
+    textAlign: 'left',
+    color: colors.search,
+  },
+  card: {
+    fontFamily: fonts.CircularBold,
+    fontsize: wp(14),
+    fontWeight: '700',
+    lineHeight: hp(14),
+    letterspacing: wp(0),
+    textAlign: 'left',
+    color: colors.white,
+  },
+  link: {
+    fontFamily: fonts.CircularMedium,
+    fontSize: wp(14),
+    fontWeight: '500',
+    lineHeight: hp(42),
+    letterSpacing: wp(0),
+    textAlign: 'right',
+    color: colors.link,
+  },
+  newsTitle: {
+    fontFamily: fonts.CircularBold,
+    fontSize: wp(14),
+    fontWeight: '700',
+    lineHeight: hp(18),
+    letterSpacing: wp(0),
+    textAlign: 'left',
+    color: colors.textTitle,
+  },
+  newsSubtitle: {
+    fontFamily: fonts.CircularBook,
+    fontSize: wp(10),
+    fontWeight: '400',
+    lineHeight: hp(13),
+    letterSpacing: wp(0),
+    textAlign: 'left',
+    color: colors.subtitle,
+  },
+  tag: {
+    fontFamily: fonts.CircularBook,
+    fontSize: wp(8),
+    fontWeight: '400',
+    lineHeight: hp(28),
+    letterSpacing: wp(0),
+    textAlign: 'center',
+    color: colors.white,
+  },
+  tagBold: {
+    fontFamily: fonts.CircularBold,
+    fontSize: wp(12),
+    fontWeight: '700',
+    lineHeight: hp(28),
+    letterSpacing: wp(0),
+    textAlign: 'center',
+    color: colors.white,
+  },
+  body: {
+    fontFamily: fonts.CircularBook,
+    fontSize: wp(14),
+    fontWeight: '400',
+    lineHeight: hp(24),
+    letterSpacing: wp(0),
+    textAlign: 'left',
+    color: colors.textTitle,
+  },
+  tabTitleActive: {
+    fontfamily: fonts.CircularBook,
+    fontSize: wp(14),
+    fontWeight: '400',
+    lineHeight: hp(28),
+    letterSpacing: wp(0),
+    textAlign: 'left',
+    color: colors.textTitle,
+  },
+  tabTitleInactive: {
+    fontfamily: fonts.CircularBook,
+    fontSize: wp(14),
+    fontWeight: '400',
+    lineHeight: hp(28),
+    letterSpacing: wp(0),
+    textAlign: 'left',
+    color: colors.search,
+  },
+  number: {
+    fontFamily: fonts.CircularBold,
+    fontSize: wp(14),
+    fontWeight: '700',
+    lineHeight: hp(14),
+    letterSpacing: wp(0),
+    textAlign: 'center',
+    color: colors.white,
+  },
+  pokemonType: {
+    fontFamily: fonts.CircularBook,
+    fontSize: wp(14),
+    fontWeight: '400',
+    lineHeight: hp(28),
+    letterSpacing: wp(0),
+    textAlign: 'right',
+    color: colors.white,
+  },
+});
