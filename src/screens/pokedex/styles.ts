78a43eac0cd58ca1cd75c6e634a37ac2c5803492
Brayan Garcia <bgarcia.95@outlook.com>
2022-05-12 16:35:45 -0600

    add pokedex ui and styles

--- src/screens/pokedex/styles.ts
+++ src/screens/pokedex/styles.ts
@@ -1,5 +1,50 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({});
+import colors from 'theme/colors';
+import {hp, wp} from 'theme/metrics';
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 60,
+    paddingHorizontal: 28,
+    zIndex: 999,
+  },
+  title: {
+    marginTop: hp(35),
+    marginHorizontal: wp(30),
+  },
+  listContainer: {
+    marginTop: hp(30),
+    flex: 1,
+  },
+  listContent: {
+    alignItems: 'center',
+    paddingBottom: hp(20),
+  },
+  fab: {
+    position: 'absolute',
+    backgroundColor: colors.link,
+    bottom: 35,
+    right: 26,
+    width: wp(45),
+    height: wp(45),
+    borderRadius: 999,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 1,
+      height: 5,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 5,
+
+    elevation: 6,
+  },
+});
 
 export default styles;
