254eca2c9ef0470c6e64eb1a15258a9014018664
Brayan Garcia <bgarcia.95@outlook.com>
2022-05-12 16:32:41 -0600

    reformat code, fix styles, add navigation to pokedex screen

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,80 +1,92 @@
 import React from 'react';
-import {
-  Image,
-  Keyboard,
-  ScrollView,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
+import {ScrollView, TouchableOpacity, View} from 'react-native';
 
-import styles from './styles';
-import globalStyles from 'theme/globalStyles';
-import colors from 'theme/colors';
+import {StackScreenProps} from '@react-navigation/stack';
+
 import {hp, wp} from 'theme/metrics';
+import styles from './styles';
+import colors from 'theme/colors';
+import {PokedexStackParams} from 'navigation/AppNavigator';
+import {Layout} from 'screens/common/Layout';
 import {CustomText} from 'components/CustomText';
 import {CustomTextInput} from 'components/CustomTextInput';
 import {CategoryCard} from 'components/CategoryCard';
 import {NewsEvent} from 'components/NewsEvent';
 
-export const HomeScreen = () => {
+interface Props extends StackScreenProps<PokedexStackParams, 'HomeScreen'> {}
+
+export const HomeScreen = ({navigation}: Props) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.imgContainer}>
-        <Image
-          source={require('~assets/images/pokeball.png')}
-          style={globalStyles.img}
-          resizeMode="cover"
-        />
-      </View>
-      <ScrollView showsVerticalScrollIndicator={false} bounces={false}>
-        <TouchableWithoutFeedback
-          style={styles.container}
-          onPress={Keyboard.dismiss}>
-          <>
-            <View style={styles.content}>
-              <CustomText style={styles.headerTitle} variant="header">
-                What Pokemon are you looking for?
-              </CustomText>
-              {/* Searchbar */}
-              <View style={styles.searchBarContainer}>
-                <CustomTextInput />
-              </View>
-              {/* Categories */}
-              <View style={styles.cardsContainer}>
-                <CategoryCard title="Pokedex" color={colors.darkGreen} />
-                <CategoryCard title="Moves" color={colors.red} />
-                <CategoryCard title="Abilities" color={colors.blue} />
-                <CategoryCard title="Items" color={colors.yellow} />
-                <CategoryCard title="Locations" color={colors.purple} />
-                <CategoryCard title="Type Charts" color={colors.brown} />
-              </View>
+    <Layout>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {/* Main Content */}
+        <View style={styles.content}>
+          <CustomText style={styles.headerTitle} variant="header">
+            What Pokemon are you looking for?
+          </CustomText>
+          {/* Searchbar */}
+          <View style={styles.searchBarContainer}>
+            <CustomTextInput />
+          </View>
+          {/* Categories */}
+          <View style={styles.cardsContainer}>
+            <CategoryCard
+              title="Pokedex"
+              color={colors.darkGreen}
+              onPress={() => navigation.navigate('PokedexScreen')}
+            />
+            <CategoryCard title="Moves" color={colors.red} />
+            <CategoryCard title="Abilities" color={colors.blue} />
+            <CategoryCard title="Items" color={colors.yellow} />
+            <CategoryCard title="Locations" color={colors.purple} />
+            <CategoryCard title="Type Charts" color={colors.brown} />
+          </View>
+
+          {/* Curve Blocks */}
+          <View style={[styles.containerCurveGray, styles.curveLeft]} />
+          <View
+            style={[
+              styles.containerCurveWhite,
+              styles.curveLeft,
+              styles.curveRotate,
+            ]}
+          />
+
+          <View style={[styles.containerCurveGray, styles.curveRight]} />
+          <View
+            style={[
+              styles.containerCurveWhite,
+              styles.curveRight,
+              styles.curveRotate,
+            ]}
+          />
+        </View>
+
+        {/* News */}
+        <View style={styles.newsContainer}>
+          <View style={{marginTop: hp(38), marginHorizontal: wp(28)}}>
+            <View style={styles.newsHeader}>
+              <CustomText variant="newsHeaderTitle">Pokemon News</CustomText>
+              <TouchableOpacity activeOpacity={0.8} onPress={() => {}}>
+                <CustomText variant="link">View all</CustomText>
+              </TouchableOpacity>
             </View>
-            {/* News */}
-            <View style={{marginTop: hp(38), marginHorizontal: wp(28)}}>
-              <View style={styles.newsHeader}>
-                <CustomText variant="newsHeaderTitle">Pokemon News</CustomText>
-                <TouchableOpacity activeOpacity={0.8} onPress={() => {}}>
-                  <CustomText variant="link">View all</CustomText>
-                </TouchableOpacity>
-              </View>
-            </View>
-            {/* News Events */}
-            <View style={{marginHorizontal: wp(28), paddingBottom: hp(20)}}>
-              <NewsEvent
-                title=" Pokémon Rumble Rush Arrives Soon"
-                date="15 May 2019"
-                img={require('~assets/images/news-1.png')}
-              />
-              <NewsEvent
-                title="Detective Pikachu Sleuths into Pokémon GO"
-                date="20 May 2019"
-                img={require('~assets/images/news-2.png')}
-              />
-            </View>
-          </>
-        </TouchableWithoutFeedback>
+          </View>
+          {/* News Events */}
+          <View style={{marginHorizontal: wp(28), paddingBottom: hp(20)}}>
+            <NewsEvent
+              title="Pokémon Rumble Rush Arrives Soon"
+              date="15 May 2019"
+              img={require('~assets/images/news-1.png')}
+            />
+            <NewsEvent
+              title="Detective Pikachu Sleuths into Pokémon GO"
+              date="20 May 2019"
+              img={require('~assets/images/news-2.png')}
+            />
+          </View>
+        </View>
       </ScrollView>
-    </View>
+    </Layout>
   );
 };
