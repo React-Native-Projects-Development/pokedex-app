bc8f6c977c6cd0f0ec12fc13f2173c4142ddc4c0
Brayan Garcia <bgarcia.95@outlook.com>
2022-05-15 13:10:24 -0600

    add styles

--- src/screens/pokemon/styles.ts
+++ src/screens/pokemon/styles.ts
@@ -1,14 +1,57 @@
+import fonts from 'config/fonts';
 import {StyleSheet} from 'react-native';
+import colors from 'theme/colors';
 
 import {hp, wp} from 'theme/metrics';
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    zIndex: -1,
+  },
   header: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     marginTop: 60,
+    height: 40,
     paddingHorizontal: 28,
+  },
+  headerText: {
+    fontFamily: fonts.CircularBlack,
+    fontSize: wp(22),
+    fontWeight: '900',
+    lineHeight: hp(28),
+    textAlign: 'center',
+    color: colors.white,
+    textTransform: 'capitalize',
+  },
+  square: {
+    position: 'absolute',
+    left: -30,
+    top: -40,
+    width: 143,
+    height: 143,
+    backgroundColor: 'rgba(255,255,255, 0.1)',
+    transform: [{rotate: '75deg'}],
+    borderRadius: 24,
+  },
+  pokemonInfoContainer: {
+    marginTop: hp(30),
+    marginHorizontal: wp(26),
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  pokemonInfoName: {
+    fontFamily: fonts.CircularBlack,
+    fontSize: wp(36),
+    fontWeight: '900',
+    textAlign: 'left',
+    color: colors.white,
+    textTransform: 'capitalize',
   },
   tagsContainer: {
     alignItems: 'flex-start',
@@ -26,6 +69,26 @@ const styles = StyleSheet.create({
   },
   tagText: {
     textTransform: 'capitalize',
+  },
+  pokeballContainer: {
+    flex: 1,
+    width: '100%',
+    position: 'absolute',
+    top: hp(20),
+    alignItems: 'center',
+    opacity: 0.4,
+  },
+  pokemonImgContainer: {
+    flex: 1,
+    width: '100%',
+    position: 'absolute',
+    top: hp(-175),
+    alignItems: 'center',
+    height: hp(225),
+  },
+  pokemonImg: {
+    width: wp(250),
+    height: hp(223),
   },
 });
 
