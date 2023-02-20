1f814a72f6f83f1a9f0520b635de6f94b0757af9
Brayan Garcia <bgarcia.95@outlook.com>
2023-02-20 15:56:38 -0600

    remove PokemonDetailsProvider

--- src/navigation/TabsNavigator.tsx
+++ src/navigation/TabsNavigator.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
 import {AboutScreen} from 'screens/tabs/AboutScreen';
 import {StatsScreen} from 'screens/tabs/StatsScreen';
 import {EvolutionScreen} from 'screens/tabs/EvolutionScreen';
@@ -8,7 +10,6 @@ import fonts from 'config/fonts';
 import {wp} from 'theme/metrics';
 import colors from 'theme/colors';
 import {CustomTabBar} from 'components/CustomTabBar';
-import {PokemonDetailsProvider} from 'context/PokemonDetailsContext';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -18,54 +19,52 @@ interface Props {
 
 export const TabsNavigator = ({id}: Props) => {
   return (
-    <PokemonDetailsProvider>
-      <Tab.Navigator
-        sceneContainerStyle={{
-          backgroundColor: colors.white,
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: colors.white,
+      }}
+      tabBar={props => <CustomTabBar {...props} />}>
+      <Tab.Screen name="About" component={AboutScreen} initialParams={{id}} />
+      <Tab.Screen
+        name="Base Stats"
+        options={{
+          tabBarLabelStyle: {
+            fontFamily: fonts.CircularMedium,
+            fontWeight: '500',
+            fontSize: wp(14),
+            textTransform: 'capitalize',
+          },
+          // tabBarLabel: 'Base Stats',
         }}
-        tabBar={props => <CustomTabBar {...props} />}>
-        <Tab.Screen name="About" component={AboutScreen} initialParams={{id}} />
-        <Tab.Screen
-          name="Base Stats"
-          options={{
-            tabBarLabelStyle: {
-              fontFamily: fonts.CircularMedium,
-              fontWeight: '500',
-              fontSize: wp(14),
-              textTransform: 'capitalize',
-            },
-            // tabBarLabel: 'Base Stats',
-          }}
-          initialParams={{id}}
-          component={StatsScreen}
-        />
-        <Tab.Screen
-          name="Evolution"
-          options={{
-            tabBarLabelStyle: {
-              fontFamily: fonts.CircularMedium,
-              fontWeight: '500',
-              fontSize: wp(14),
-              textTransform: 'capitalize',
-            },
-          }}
-          initialParams={{id}}
-          component={EvolutionScreen}
-        />
-        <Tab.Screen
-          name="Moves"
-          options={{
-            tabBarLabelStyle: {
-              fontFamily: fonts.CircularMedium,
-              fontWeight: '500',
-              fontSize: wp(14),
-              textTransform: 'capitalize',
-            },
-          }}
-          initialParams={{id}}
-          component={MovesScreen}
-        />
-      </Tab.Navigator>
-    </PokemonDetailsProvider>
+        initialParams={{id}}
+        component={StatsScreen}
+      />
+      <Tab.Screen
+        name="Evolution"
+        options={{
+          tabBarLabelStyle: {
+            fontFamily: fonts.CircularMedium,
+            fontWeight: '500',
+            fontSize: wp(14),
+            textTransform: 'capitalize',
+          },
+        }}
+        initialParams={{id}}
+        component={EvolutionScreen}
+      />
+      <Tab.Screen
+        name="Moves"
+        options={{
+          tabBarLabelStyle: {
+            fontFamily: fonts.CircularMedium,
+            fontWeight: '500',
+            fontSize: wp(14),
+            textTransform: 'capitalize',
+          },
+        }}
+        initialParams={{id}}
+        component={MovesScreen}
+      />
+    </Tab.Navigator>
   );
 };
