f99c1954100d747a36f62327ffc68d6db13f3721
Brayan Garcia <bgarcia.95@outlook.com>
2023-02-20 15:57:16 -0600

    display animated menu

--- src/screens/pokedex/PokedexScreen.tsx
+++ src/screens/pokedex/PokedexScreen.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {
   ActivityIndicator,
+  Animated,
   FlatList,
   TouchableOpacity,
   View,
@@ -19,14 +20,77 @@ import FilterIcon from 'components/icons/FilterIcon';
 import {Layout} from 'screens/common/Layout';
 import {CustomText} from 'components/CustomText';
 import {PokeCard} from 'components/PokeCard';
+import CloseIcon from 'components/icons/CloseIcon';
+import PokeballIcon from 'components/icons/PokeballIcon';
+import MagnifierIcon from 'components/icons/MagnifierIcon';
+import HeartFilledIcon from 'components/icons/HeartFilledIcon';
 
 interface Props extends StackScreenProps<PokedexStackParams, 'PokedexScreen'> {}
+
+const Overlay = ({opacity, isLayoutVisible}: any) => {
+  return (
+    <Animated.View
+      style={{
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 1,
+        opacity,
+        display: isLayoutVisible ? 'flex' : 'none',
+      }}
+    />
+  );
+};
 
 export const PokedexScreen = ({navigation}: Props) => {
   const {isLoading, loadPokemons, pokemons} = usePaginatedPokemons();
 
+  const [isLayoutVisible, setIsLayoutVisible] = useState(false);
+
+  const onToggleLayout = () => setIsLayoutVisible(prevState => !prevState);
+
+  const animation = useRef(new Animated.Value(0)).current;
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  const onFadeIn = () => {
+    return Animated.timing(opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const onFadeOut = () => {
+    return Animated.timing(opacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const onShow = () => {
+    return Animated.spring(animation, {
+      toValue: 1,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const onHide = () => {
+    return Animated.spring(animation, {
+      toValue: 0,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <Layout>
+      <Overlay opacity={opacity} isLayoutVisible={isLayoutVisible} />
+
       <View style={styles.header}>
         <TouchableOpacity
           activeOpacity={0.8}
@@ -35,7 +99,7 @@ export const PokedexScreen = ({navigation}: Props) => {
         </TouchableOpacity>
         <TouchableOpacity
           activeOpacity={0.8}
-          onPress={() => console.log('pressed')}>
+          onPress={() => console.log('categories pressed')}>
           <ListIcon width={wp(18)} height={hp(16)} />
         </TouchableOpacity>
       </View>
@@ -49,9 +113,7 @@ export const PokedexScreen = ({navigation}: Props) => {
           numColumns={2}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={styles.listContent}
-          renderItem={({item, index}) => {
-            return <PokeCard item={item} index={index} />;
-          }}
+          renderItem={({item, index}) => <PokeCard item={item} index={index} />}
           // Infinite Scroll
           onEndReached={loadPokemons}
           onEndReachedThreshold={0.5}
@@ -61,11 +123,198 @@ export const PokedexScreen = ({navigation}: Props) => {
             ) : null
           }
         />
+      </View>
+      <View style={{zIndex: 4, width: '100%'}}>
+        <View
+          style={{
+            position: 'absolute',
+            bottom: hp(94),
+            right: wp(25),
+            width: '100%',
+            alignItems: 'flex-end',
+          }}>
+          <TouchableOpacity activeOpacity={0.9}>
+            <Animated.View
+              style={{
+                backgroundColor: colors.white,
+                borderRadius: wp(33),
+                // marginBottom: hp(9),
+                paddingHorizontal: wp(15),
+                paddingVertical: hp(8),
+                // width: '50%',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                // opacity: fadeAnim,
+                position: 'absolute',
+                right: 0,
+                transform: [
+                  {
+                    scale: animation,
+                  },
+                  {
+                    translateY: animation.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, -190],
+                    }),
+                  },
+                  {
+                    translateX: animation.interpolate({
+                      inputRange: [0, 0.5, 1],
+                      outputRange: [0, isLayoutVisible ? -250 : 0, 0],
+                    }),
+                  },
+                ],
+              }}>
+              <CustomText variant="body" style={{marginRight: wp(14)}}>
+                Favorite
+              </CustomText>
+              <HeartFilledIcon
+                width={wp(20)}
+                height={hp(20)}
+                color={colors.link}
+              />
+            </Animated.View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.9}>
+            <Animated.View
+              style={{
+                backgroundColor: colors.white,
+                borderRadius: wp(33),
+                // marginBottom: hp(9),
+                paddingHorizontal: wp(15),
+                paddingVertical: hp(8),
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                // opacity: fadeAnim,
+                position: 'absolute',
+                right: 0,
+                transform: [
+                  {
+                    scale: animation,
+                  },
+                  {
+                    translateY: animation.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, -140],
+                    }),
+                  },
+                  {
+                    translateX: animation.interpolate({
+                      inputRange: [0, 0.5, 1],
+                      outputRange: [0, isLayoutVisible ? -200 : 0, 0],
+                    }),
+                  },
+                ],
+              }}>
+              <CustomText variant="body" style={{marginRight: wp(12)}}>
+                All Type
+              </CustomText>
+              <PokeballIcon width={wp(20)} height={hp(20)} />
+            </Animated.View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.9}>
+            <Animated.View
+              style={{
+                backgroundColor: colors.white,
+                borderRadius: wp(33),
+                // marginBottom: hp(9),
+                paddingHorizontal: wp(15),
+                paddingVertical: hp(8),
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                // opacity: fadeAnim,
+                position: 'absolute',
+                right: 0,
+                transform: [
+                  {
+                    scale: animation,
+                  },
+                  {
+                    translateY: animation.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, -90],
+                    }),
+                  },
+                  {
+                    translateX: animation.interpolate({
+                      inputRange: [0, 0.5, 1],
+                      outputRange: [0, isLayoutVisible ? -150 : 0, 0],
+                    }),
+                  },
+                ],
+              }}>
+              <CustomText variant="body" style={{marginRight: wp(12)}}>
+                All Gen
+              </CustomText>
+              <PokeballIcon width={wp(20)} height={hp(20)} />
+            </Animated.View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.9}>
+            <Animated.View
+              style={{
+                backgroundColor: colors.white,
+                borderRadius: wp(33),
+                // marginBottom: hp(9),
+                paddingHorizontal: wp(15),
+                paddingVertical: hp(8),
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                opacity: animation,
+                position: 'absolute',
+                right: 0,
+                transform: [
+                  // {
+                  //   scale: fadeAnim,
+                  // },
+                  {
+                    translateY: animation.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, -40],
+                    }),
+                  },
+                  {
+                    translateX: animation.interpolate({
+                      inputRange: [0, 0.5, 1],
+                      outputRange: [0, isLayoutVisible ? -100 : 0, 0],
+                    }),
+                  },
+                ],
+              }}>
+              <CustomText variant="body" style={{marginRight: wp(12)}}>
+                Search
+              </CustomText>
+              <MagnifierIcon
+                width={wp(20)}
+                height={hp(20)}
+                color={colors.link}
+              />
+            </Animated.View>
+          </TouchableOpacity>
+        </View>
+
         <TouchableOpacity
           activeOpacity={0.9}
-          onPress={() => {}}
+          onPress={() => {
+            onToggleLayout();
+
+            if (!isLayoutVisible) {
+              onFadeIn();
+              onShow();
+            } else {
+              onHide();
+              onFadeOut();
+            }
+          }}
           style={styles.fab}>
-          <FilterIcon />
+          {isLayoutVisible ? (
+            <CloseIcon width={wp(14)} height={hp(14)} />
+          ) : (
+            <FilterIcon />
+          )}
         </TouchableOpacity>
       </View>
     </Layout>
